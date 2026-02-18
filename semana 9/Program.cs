aa22e4c7d3a374467e1f724f42be5094c803fddd
ariel zurita <as.zuritaq@uea.edu.ec>
2026-02-17 21:18:04 -0500

    Add files via upload

---
+++ semana 9/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Conjunto universal: 500 ciudadanos
+        HashSet<string> ciudadanos = new HashSet<string>();
+
+        for (int i = 1; i <= 500; i++)
+        {
+            ciudadanos.Add("Ciudadano " + i);
+        }
+
+        // Conjunto Pfizer (75 ciudadanos)
+        HashSet<string> pfizer = new HashSet<string>();
+
+        for (int i = 1; i <= 75; i++)
+        {
+            pfizer.Add("Ciudadano " + i);
+        }
+
+        // Conjunto AstraZeneca (75 ciudadanos)
+        HashSet<string> astraZeneca = new HashSet<string>();
+
+        for (int i = 50; i <= 124; i++)
+        {
+            astraZeneca.Add("Ciudadano " + i);
+        }
+
+        // Ambas dosis (Intersección)
+        var ambasDosis = pfizer.Intersect(astraZeneca);
+
+        // Unión de vacunados
+        var vacunados = pfizer.Union(astraZeneca);
+
+        // No vacunados
+        var noVacunados = ciudadanos.Except(vacunados);
+
+        // Solo Pfizer
+        var soloPfizer = pfizer.Except(astraZeneca);
+
+        // Solo AstraZeneca
+        var soloAstraZeneca = astraZeneca.Except(pfizer);
+
+        // Mostrar resultados
+        Console.WriteLine("===== CIUDADANOS CON AMBAS DOSIS =====");
+        foreach (var c in ambasDosis)
+            Console.WriteLine(c);
+
+        Console.WriteLine("\n===== CIUDADANOS NO VACUNADOS =====");
+        foreach (var c in noVacunados)
+            Console.WriteLine(c);
+
+        Console.WriteLine("\n===== CIUDADANOS SOLO PFIZER =====");
+        foreach (var c in soloPfizer)
+            Console.WriteLine(c);
+
+        Console.WriteLine("\n===== CIUDADANOS SOLO ASTRAZENECA =====");
+        foreach (var c in soloAstraZeneca)
+            Console.WriteLine(c);
+    }
+}
+
